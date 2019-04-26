a84d60dd75d3f9bd2c66f19b96bb3b15f2e27267
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2019-04-26 17:56:08 +0530

    add more datasets

--- opencv.py
+++ opencv.py
@@ -2,14 +2,6 @@ import face_recognition
 import cv2
 import numpy as np
 
-# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
-# other example, but it includes some basic performance tweaks to make things run a lot faster:
-#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
-#   2. Only detect faces in every other frame of video.
-
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
 
 # Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
@@ -22,14 +14,27 @@ akhil_face_encoding = face_recognition.face_encodings(akhil_image)[0]
 ganesh_image = face_recognition.load_image_file("img/ganesh.jpg")
 ganesh_face_encoding = face_recognition.face_encodings(ganesh_image)[0]
 
+abin_image = face_recognition.load_image_file("img/abin.jpg")
+abin_face_encoding = face_recognition.face_encodings(abin_image)[0]
+
+jicku_image = face_recognition.load_image_file("img/jicku.jpg")
+jicku_face_encoding = face_recognition.face_encodings(jicku_image)[0]
+
+
+
+
 # Create arrays of known face encodings and their names
 known_face_encodings = [
     akhil_face_encoding,
-    ganesh_face_encoding
+    ganesh_face_encoding,
+    abin_face_encoding,
+    jicku_face_encoding
 ]
 known_face_names = [
     "Akhil G",
-    "Ganesh U"
+    "Ganesh U",
+    "Abin",
+    "Jicku"
 ]
 
 # Initialize some variables
